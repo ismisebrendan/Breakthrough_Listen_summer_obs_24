2af550b486e039e31a0b804379ea755ddf490069
Brendan <135332281+ismisebrendan@users.noreply.github.com>
2024-07-08 09:00:59 +0100

    Update scheduling.py
    Made to make only use default parameters

--- scheduling.py
+++ scheduling.py
@@ -19,64 +19,19 @@ import datetime as dt
 # Timings #
 ###########
 
-# Check if default summer observation parameters are to be used
-default = False
 print('--- DEFAULT PARAMETERS ---')
 print('Observe from 20:00 UTC Tuesday to 03:59 UTC Wednesday')
 print('First observe a test pulsar for 30 minutes')
 print('Observe exoplanets for 80 minutes each for the rest of the time')
 
-while default == False:
 
-    lof = input('Use default parameters? [Y/N]').upper()
+# Find the dates of the next Tuesday (including possibly today) and Wednesday
+next_Tuesday_days = (1 - dt.datetime.today().weekday()) % 7
+next_Tuesday = dt.datetime.today() + dt.timedelta(days=next_Tuesday_days)
+next_Wednesday = dt.datetime.today() + dt.timedelta(days=next_Tuesday_days + 1)
 
-    if lof == 'Y':
-        default = True
-
-        # Find the dates of the next Tuesday (including possibly today) and Wednesday
-        next_Tuesday_days = (1 - dt.datetime.today().weekday()) % 7
-        next_Tuesday = dt.datetime.today() + dt.timedelta(days=next_Tuesday_days)
-        next_Wednesday = dt.datetime.today() + dt.timedelta(days=next_Tuesday_days + 1)
-
-        starting = next_Tuesday.strftime('%Y-%m-%d') + ' 20:00'
-        ending = next_Wednesday.strftime('%Y-%m-%d') + ' 03:59'
-
-    elif lof == 'N':
-        break
-    else:
-        print('Please only type "Y" or "N"')
-
-# If making manual observations
-if default == False:
-
-    valid_dates = False
-    valid_times = False
-    # Check that dates and times are in the right order and formats
-    while valid_dates == False:
-        start_date = check_date('Please enter starting date of observation (YYYY-MM-DD): ')
-        end_date = check_date('Please enter ending date of observation (YYYY-MM-DD): ')
-
-        if (int(start_date[0:4]) > int(end_date[0:4])) or (int(start_date[0:4]) == int(end_date[0:4]) and int(start_date[5:7]) > int(end_date[5:7])) or (start_date[0:4] == end_date[0:4] and start_date[5:7] == end_date[5:7] and int(start_date[8:10]) > int(end_date[8:10])):
-            print('End date must be the same or later than the start date')
-        else:
-            valid_dates = True
-
-    while valid_times == False:
-        start_time = check_time('Please enter starting time of observation (HH:MM): ')
-        end_time = check_time('Please enter ending time of observation (HH:MM): ')
-
-        if start_date == end_date:
-            if (start_time == end_time) or (int(start_time[0:2]) > int(end_time[0:2])) or (int(start_time[0:2]) == int(end_time[0:2]) and int(start_time[3:5]) > int(end_time[3:5])):
-                print('End time must be later than the start time')
-            else:
-                valid_times = True
-        else:
-            valid_times = True
-
-
-    starting = start_date + ' ' + start_time
-    ending = end_date + ' ' + end_time
-
+starting = next_Tuesday.strftime('%Y-%m-%d') + ' 20:00'
+ending = next_Wednesday.strftime('%Y-%m-%d') + ' 03:59'
 starting_time = Time(starting, scale='utc')
 ending_time = Time(ending, scale='utc')
 
@@ -102,23 +57,12 @@ print('Starting LST at midpoint:', LST_start_mid)
 print(f'Ending at {ending} UTC')
 print('Ending LST at midpoint:', LST_end_mid)
 
-# Check what objects are to be observed
-if default == True:
-    want_exo = True
-    want_psr = False
-else:
-    want_exo = want('exoplanets')
-    want_psr = want('pulsars independently')
-
 ######################################
 # Import pulsar data and clean it up #
 ######################################
 
-# Need to at least observe a pulsar once for calibration purposes
-if default == True:
-    pulsars = ascii.read('pulsars.csv')
-else:
-    pulsars = check_file('Path to CSV file containing pulsar names, coordinates and luminosities in ATNF format: ', ['NAME', 'RAJ', 'DECJ', 'R_LUM'])
+# Need to observe a pulsar once for calibration purposes
+pulsars = ascii.read('pulsars.csv')
 
 # Remove rows without luminosities
 ind = pulsars['R_LUM'] != '*'
@@ -133,32 +77,12 @@ for i in range(len(pulsars['R_LUM'])):
 pulsars['R_LUM'] = luminosities
 
 # The positions of the pulsars
-psr_ra = np.array(pulsars['RAJ'])
-psr_dec = np.array(pulsars['DECJ'])
-psr_coords = SkyCoord(ra=psr_ra, dec=psr_dec, unit=(u.hourangle, u.deg))
+psr_coords = SkyCoord(ra=np.array(pulsars['RAJ']), dec=np.array(pulsars['DECJ']), unit=(u.hourangle, u.deg))
 
 # Pointing times
-if default == True:
-    pointing_time_psr_cal = 0.5 + 1/60
-    pointing_time_planet = 4/3 + 1/60
-    planets = Table(names=['hostname', 'ra', 'dec', 'sy_dist'], dtype=[str, float, float, float])
-else:
-    # If manual ask for the pointing times
-    pointing_time_psr_cal = check_time('Time to spend observing a calibration pulsar (HH:MM): ')
-    pointing_time_psr_cal = (int(pointing_time_psr_cal[0:2])*u.hr + int(pointing_time_psr_cal[3:5])*u.min + 1*u.min).to(u.hr).value
-
-    if want_exo == True:
-        pointing_time_planet = check_time('Time to spend observing a planet (HH:MM): ')
-        pointing_time_planet = (int(pointing_time_planet[0:2])*u.hr + int(pointing_time_planet[3:5])*u.min + 1*u.min).to(u.hr).value
-        planets = Table(names=['hostname', 'ra', 'dec', 'sy_dist'], dtype=[str, float, float, float])
-    if want_psr == True:
-        pointing_time_psr = check_time('Time to spend observing a pulsar (HH:MM): ')
-        pointing_time_psr = (int(pointing_time_psr[0:2])*u.hr + int(pointing_time_psr[3:5])*u.min + 1*u.min).to(u.hr).value
-
-        # Copy of pulsars that we can delete stuff from
-        pulsars_copy = Table(pulsars, copy=True)
-
-        psr_coords_copy = SkyCoord(ra=np.array(pulsars_copy['RAJ']), dec=np.array(pulsars_copy['DECJ']), unit=(u.hourangle, u.deg))
+pointing_time_psr_cal = 0.5 + 1/60
+pointing_time_planet = 4/3 + 1/60
+planets = Table(names=['hostname', 'ra', 'dec', 'sy_dist'], dtype=[str, float, float, float])
 
 # List of targets to ignore (uusally because they had been observed previously)
 try:
@@ -192,12 +116,9 @@ ind = np.argmin(sep)
 target_list.append(pulsars['NAME'][ind])
 target_type.append('pulsar')
 
-# Delete that index from the coordinates array and the copy table to prevent repeats if necessary
-try:
-    psr_coords_copy = np.delete(psr_coords_copy, ind)
-    pulsars_copy.remove_row(ind)
-except:
-    pass
+# Remove all other pulsars from pulsars and psr_coords
+pulsars = pulsars[ind]
+psr_coords = psr_coords[ind]
 
 # Move the time on
 time_offset += pointing_time_psr_cal
@@ -210,71 +131,43 @@ while time_offset <= obs_time:
     # Find the RA that is directly overhead in the middle of the observation window
     # Choose to observe the target that is closest to the zenith at the middle of the observation window
 
-    # Exoplanets
-    if want_exo == True:
-        # Find zenith in the middle of observing
-        mid_lst_planet = (time_LST + (pointing_time_planet - 1/60)/2) * 15
-        zenith_planet = SkyCoord(ra=mid_lst_planet, dec=mid_lat, unit='deg')
+    # Find zenith in the middle of observing
+    mid_lst = (time_LST + (pointing_time_planet - 1/60)/2) * 15
+    zenith = SkyCoord(ra=mid_lst, dec=mid_lat, unit='deg')
 
-        # Query for list of planets to target near the zenith
-        potential_targets = NasaExoplanetArchive.query_region(table='pscomppars', coordinates=zenith_planet, radius=20*u.deg)
-        potential_targets.keep_columns(('hostname', 'disc_facility', 'ra', 'dec', 'sy_dist'))
+    # Query for list of planets to target near the zenith
+    potential_targets = NasaExoplanetArchive.query_region(table='pscomppars', coordinates=zenith, radius=20*u.deg)
+    potential_targets.keep_columns(('hostname', 'disc_facility', 'ra', 'dec', 'sy_dist'))
 
-        # Only keep TESS targets
-        ind = potential_targets['disc_facility'] == 'Transiting Exoplanet Survey Satellite (TESS)'
+    # Only keep TESS targets
+    ind = potential_targets['disc_facility'] == 'Transiting Exoplanet Survey Satellite (TESS)'
+    potential_targets = potential_targets[ind]
+
+    # Delete the specified targets to be ignored
+    for i in range(len(ignore)):
+        ind = potential_targets['hostname'] != ignore['name'][i]
+        potential_targets = potential_targets[ind]
+    
+    # Make sure same target is not observed multiple times in one session
+    for i in range(len(target_list)):
+        ind = potential_targets['hostname'] != target_list[i]
         potential_targets = potential_targets[ind]
 
-        # Delete the specified targets to be ignored
-        for i in range(len(ignore)):
-            ind = potential_targets['hostname'] != ignore['name'][i]
-            potential_targets = potential_targets[ind]
-        
-        # Make sure same target is not observed multiple times in one session
-        for i in range(len(target_list)):
-            ind = potential_targets['hostname'] != target_list[i]
-            potential_targets = potential_targets[ind]
+    # The coordinates of the planets
+    planets_coords = SkyCoord(ra=potential_targets['ra'], dec=potential_targets['dec'], unit='deg')
 
-        # The coordinates of the planets
-        planets_coords = SkyCoord(ra=potential_targets['ra'], dec=potential_targets['dec'], unit='deg')
+    sep = zenith.separation(planets_coords)
+    ind = np.argmin(sep)
+    sep = sep[ind]
 
-        sep_exo = zenith_planet.separation(planets_coords)
-        ind_exo = np.argmin(sep_exo)
-        sep_exo = sep_exo[ind_exo]
-    else:
-        sep_exo = np.inf
-
-    # Pulsars
-    if want_psr == True:
-        mid_lst_psr = (time_LST + (pointing_time_psr - 1/60)/2) * 15
-        zenith_psr = SkyCoord(ra=mid_lst_psr, dec=mid_lat, unit='deg')
-        sep_psr = zenith_psr.separation(psr_coords_copy)
-        ind_psr = np.argmin(sep_psr)
-        sep_psr = sep_psr[ind_psr]
-    else:
-        sep_psr = np.inf
-
-    # See which is closest to zenith
-    if sep_exo < sep_psr:
-        target_list.append(potential_targets['hostname'][ind_exo])
-        target_type.append('planet')
-
-        planets.add_row((potential_targets['hostname'][ind_exo], potential_targets['ra'][ind_exo], potential_targets['dec'][ind_exo], potential_targets['sy_dist'][ind_exo]))
-        
-        # Move on
-        time_offset += pointing_time_planet
-        time_LST += pointing_time_planet
-    else:
-        target_list.append(pulsars_copy['NAME'][ind_psr])
-
-        # Delete that index from the coordinates array and the copy table to prevent repeats
-        psr_coords_copy = np.delete(psr_coords_copy, ind_psr)
-        pulsars_copy.remove_row(ind_psr)
-
-        target_type.append('pulsar')
-        
-        # Move on
-        time_offset += pointing_time_psr
-        time_LST += pointing_time_psr
+    # Record this planet
+    target_list.append(potential_targets['hostname'][ind])
+    target_type.append('planet')
+    planets.add_row((potential_targets['hostname'][ind], potential_targets['ra'][ind], potential_targets['dec'][ind], potential_targets['sy_dist'][ind]))
+    
+    # Move on
+    time_offset += pointing_time_planet
+    time_LST += pointing_time_planet
 
 #############
 #           #
@@ -294,20 +187,8 @@ for i in range(len(planets)):
     # Plot from optimum start time of observation
     ax1.broken_barh([(planets['ra'][i] - (pointing_time_planet - 1/60) * 7.5, (pointing_time_planet - 1/60) * 15)], (planets['sy_dist'][i], 10), color='red')
 
-# All pulsars
-for i in range(len(pulsars['NAME'])):
-    # Plot from optimum start time of observation
-    try:
-        # Plot the length of the pointing time at a stanfalone pulsar if possible
-        ax2.broken_barh([(Angle(psr_ra[i], u.hourangle).value * 15 - (pointing_time_psr - 1/60) * 7.5, (pointing_time_psr_cal - 1/60) * 15)], (pulsars['R_LUM'][i], 10), color='green')
-    except:
-        # If not plot for the length of the pulsar calibration pointing time
-        ax2.broken_barh([(Angle(psr_ra[i], u.hourangle).value * 15 - (pointing_time_psr_cal - 1/60) * 7.5, (pointing_time_psr_cal - 1/60) * 15)], (pulsars['R_LUM'][i], 10), color='green')
-
-# Plot the calibration pulsar in a different colour
-index = pulsars['NAME'] == target_list[0]
-ind = np.argwhere(index)[0][0]
-ax2.broken_barh([(Angle(psr_ra[ind], u.hourangle).value * 15 - (pointing_time_psr_cal - 1/60) * 7.5, (pointing_time_psr_cal - 1/60) * 15)], (pulsars['R_LUM'][ind], 10), color='purple')
+# Plot the calibration pulsar
+ax2.broken_barh([(Angle(psr_coords[0].ra, u.hourangle).value * 15 - (pointing_time_psr_cal - 1/60) * 7.5, (pointing_time_psr_cal - 1/60) * 15)], (pulsars['R_LUM'][0], 10), color='green')
         
 ax1.set_title('Optimum observation windows')
 ax1.set_xticks(np.arange(0, 361, 30))
@@ -328,12 +209,9 @@ fig, ax1 = plt.subplots(figsize=(8,6))
 current_LST = LST_start_mid.value * 360/24
 end_LST = LST_end_mid.value * 360/24
 
-# Calibration pulsar in a different colour
-index = pulsars['NAME'] == target_list[0]
-ind = np.argwhere(index)[0][0]
-
-ax1.broken_barh([(current_LST, pointing_time_psr_cal * 15)], (0, 10), color='purple')
-ax1.text(current_LST, 5, pulsars['NAME'][ind])
+# Calibration pulsar
+ax1.broken_barh([(current_LST, pointing_time_psr_cal * 15)], (0, 10), color='green')
+ax1.text(current_LST, 5, pulsars['NAME'][0])
 ax1.plot(psr_coords[ind].ra.value, 0, 'bo')
 
 current_LST += pointing_time_psr_cal * 15
@@ -370,29 +248,27 @@ plt.savefig('viewing.png')
 ################
 
 # Take same frequency range for all observations
-
 freq_range = '100e6:190e6'
 
+# Table for output
 sched_iLiSA = Table(names=('Name', 'Time', 'RA', 'DEC', 'freqrng', 'dur'), dtype=(str, str, float, float, str, str))
 
+# Starting time
 time = starting_time.mjd
 
-index = pulsars['NAME'] == target_list[0]
-ind = np.argwhere(index)[0][0]
+# Record the calibraton pulsar
+sched_iLiSA.add_row((pulsars['NAME'][0], Time(time, format='mjd').iso[11:16], psr_coords[0].ra.value*u.deg.to(u.rad), psr_coords[0].dec.value*u.deg.to(u.rad), freq_range, str(int(np.round((pointing_time_psr_cal - 1/60) * 60))) + 'm'))
 
-sched_iLiSA.add_row((pulsars['NAME'][0], Time(time, format='mjd').iso[11:16], psr_coords[ind].ra.value*u.deg.to(u.rad), psr_coords[ind].dec.value*u.deg.to(u.rad), freq_range, str(int(np.round((pointing_time_psr_cal - 1/60) * 60))) + 'm'))
-
+# Wait for the 31 minutes
 time += pointing_time_psr_cal/24
 
+# Add the planets
 for i in range(1, len(target_list)):
-    index = planets['hostname'] == target_list[i]
-    ind = np.argwhere(index)[0][0]
-
     if i == len(target_list) - 1:
         dur = np.round((ending_time.mjd - time)*u.day.to(u.min))
-        sched_iLiSA.add_row((planets['hostname'][ind], Time(time, format='mjd').iso[11:16], planets['ra'][ind]*u.deg.to(u.rad), planets['dec'][ind]*u.deg.to(u.rad), freq_range, str(dur) + 'm'))
+        sched_iLiSA.add_row((planets['hostname'][i], Time(time, format='mjd').iso[11:16], planets['ra'][i]*u.deg.to(u.rad), planets['dec'][i]*u.deg.to(u.rad), freq_range, str(dur) + 'm'))
     else:
-        sched_iLiSA.add_row((planets['hostname'][ind], Time(time, format='mjd').iso[11:16], planets['ra'][ind]*u.deg.to(u.rad), planets['dec'][ind]*u.deg.to(u.rad), freq_range, str(int(np.round((pointing_time_planet - 1/60) * 60))) + 'm'))
+        sched_iLiSA.add_row((planets['hostname'][i], Time(time, format='mjd').iso[11:16], planets['ra'][i]*u.deg.to(u.rad), planets['dec'][i]*u.deg.to(u.rad), freq_range, str(int(np.round((pointing_time_planet - 1/60) * 60))) + 'm'))
 
     # Wait the 1 hr 21 minutes
     time += pointing_time_planet/24
